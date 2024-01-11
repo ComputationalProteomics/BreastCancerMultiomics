ea798ebcbe3423136542c6a44c557bf9c5f47f46
sergiomosquim <51542516+sergiomosquim@users.noreply.github.com>
2024-01-11 11:54:14 +0100

    Add id_sorting script

--- notebooks/id_sorting.R
+++ notebooks/id_sorting.R
@@ -1,12 +1,4 @@
-#!/usr/bin/env Rscript
-
-library(tidyverse)
-
-sortIds <- function(){
-  argv <- parse_input_params()
-  
-  data <- readr::read_tsv(file = argv$data_path)
-  proteinColumn <- argv$protein_col
+sortIds <- function(data,proteinColumn = 'Protein.Ids',...){
   data <- pull(data,proteinColumn) %>% 
     str_split(string = .,pattern = ';') %>% 
     map(.x = .,.f = str_sort) %>% 
@@ -15,23 +7,5 @@ sortIds <- function(){
     mutate(data,Protein.ID=.,.before=colnames(data[2])) %>% 
     dplyr::select(-proteinColumn)
   names(data)[names(data)=='Protein.ID'] <- 'Protein'
-  
-  readr::write_tsv(x = data,file = argv$out_path)
-}
-
-
-parse_input_params <- function() {
-  
-  parser <- argparser::arg_parser("Sorting IDs")
-  parser <- argparser::add_argument(parser, "--data_path", help="Data matrix path", type="character", default=NA,nargs = 1)
-  parser <- argparser::add_argument(parser, "--out_path", help="Output matrix path", type="character", nargs=1)
-  parser <- argparser::add_argument(parser, "--protein_col", help="Protein column in main data frame", type="character", default='Protein.Ids')
-
-  argv <- argparser::parse_args(parser)
-  
-  argv
-}
-
-if (!interactive()) {
-  sortIds()
+  return(data)
 }

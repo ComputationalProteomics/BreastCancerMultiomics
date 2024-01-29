fb38d244762827d0c57bb07eb648e85a18f512c9
sergiomosquim <51542516+sergiomosquim@users.noreply.github.com>
2024-01-30 00:50:44 +0100

    Add missing value based filtering for DIABLO model

--- notebooks/medianFiltering.R
+++ notebooks/medianFiltering.R
@@ -17,3 +17,29 @@ medianFiltering <- function(data,design,sampleCol,annotation,cutoff=0.5,...){
   
   return(lowVarData)
 }
+
+
+# Function to filter rows after a threshold of NA values
+NAFiltering <- function(data,design,sampleCol,cutoff=0.3,...){
+  countNA <- function(x) {
+    naCount <- is.na(x) %>% sum()
+    cutoff <- naCount/length(x)
+    return(cutoff)
+  }
+  
+  NAData <- data %>% 
+    dplyr::select(design[[sampleCol]]) %>% 
+    t %>% 
+    as_tibble %>% 
+    map_dfc(.,countNA) %>% 
+    t
+  
+  dataNA <- data %>% 
+    mutate(number.NA=NAData)
+  
+  filteredData <- dataNA %>% 
+    filter(number.NA <= cutoff) %>% 
+    dplyr::select(-c('number.NA'))
+  
+  return(filteredData)
+}

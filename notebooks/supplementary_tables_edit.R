adf7c90a48dfd1cce66f26af4744209cfdc5bff9
sergiomosquim <51542516+sergiomosquim@users.noreply.github.com>
2024-07-25 22:35:23 +0200

    Notebook to generate and format the supplementary tables for the manuscript. It is not final

---
+++ notebooks/supplementary_tables_edit.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+data1 <- read_tsv("~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_2.tsv")
+data2 <- read_tsv("~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_3.tsv")
+
+teste <- inner_join(data1,data2,by = "variable",suffix = c(x=".RFS",y=".OS"))
+teste <- teste %>% filter(p.value_univariable.RFS<0.05 & p.value_univariable.OS<0.05)
+teste <- teste %>% select(variable,15:27,41:53)
+write_tsv(x = teste,file = "~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_2_3_joined.tsv")
+
+data1 <- read_tsv("~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_4.tsv")
+data2 <- read_tsv("~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_5.tsv")
+teste <- inner_join(data1,data2,by = "variable",suffix = c(x=".DRFS",y=".OS"))
+teste <- teste %>% select(variable,14,18,24,39,43,49)
+write_tsv(x = teste,file = "~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_4_5_joined.tsv")
+
+data1 <- read_tsv("~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_6.tsv")
+data2 <- read_tsv("~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_7.tsv")
+teste <- inner_join(data1,data2,by = "variable",suffix = c(x=".DRFS",y=".OS"))
+teste <- teste %>% filter(p.value_univariable.DRFS<0.05 | p.value_univariable.OS<0.05)
+teste <- teste %>% select(variable,15,19,25,41,45,51)
+write_tsv(x = teste,file = "~/Library/Mobile Documents/com~apple~CloudDocs/thesis/manuscript_figures/supplementary_table_6_7_joined.tsv")
